4c1b431f78658a97eff42d99d61e4ed11cb70e2c
dv-sandeep <121549648+dv-sandeep@users.noreply.github.com>
2023-03-02 22:57:51 +0530

    Update viplist.py

--- viplist.py
+++ viplist.py
@@ -5,11 +5,6 @@ import getpass as gp
 
 def vip_info(vip):
     vip_name = "tmsh list ltm virtual one-line |grep " + vip + " | gawk {'print $3'}"
-    
     conn = sshconnect.connect(sshconnect.ip, sshconnect.user, sshconnect.passwrd)
     vs_name = execcommand.exec_cmd(conn, vip_name)
-    
-    print(vs_name)
-    return 0
-
-vip_info("10.10.20.55:4433")
+    return vs_name
